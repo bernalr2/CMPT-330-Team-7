13eed9db7e35b7a6bc5950cb9bfe71fb3924aeb3
Ryan <bernalr2@mymacewan.ca>
2025-03-18 10:25:58 -0600

    Fixed Tab Colours and Switching in Menu UI
    - Fixed an issue in The Investigation where the Tabs when you opened up the menus was gray and white instead of their original colours. This has been fixed to reflect the tab colours featured on The Autopsy Report Fragment

--- CMPT 330 Team 7 Project/Assets/Scripts/InvestigationMenu/TabsController.cs
+++ CMPT 330 Team 7 Project/Assets/Scripts/InvestigationMenu/TabsController.cs
@@ -1,10 +1,13 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TabsController : MonoBehaviour
 {
     public Image[] tabImages;
-    public GameObject[] pages; 
+    public GameObject[] pages;
+    public Color originalColor;
+    public Color savedColor;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,10 +19,25 @@ public class TabsController : MonoBehaviour
     {
         for(int i = 0; i < pages.Length; i++)
         {
+            switch(i)
+            {
+                case 0:
+                    originalColor = Color.yellow;
+                    break;
+                case 1:
+                    originalColor = Color.red;
+                    break;
+                case 2:
+                    originalColor = Color.green;
+                    break;
+                case 3:
+                    originalColor = Color.blue;
+                    break;
+            }
             pages[i].SetActive(false);
-            tabImages[i].color = Color.gray;
+            tabImages[i].color = new Color(originalColor.r * 0.5f, originalColor.g * 0.5f, originalColor.b * 0.5f);
         }
         pages[tabNo].SetActive(true);
-        tabImages[tabNo].color = Color.white;
+        tabImages[tabNo].color = new Color(tabImages[tabNo].color.r / 0.5f, tabImages[tabNo].color.g / 0.5f, tabImages[tabNo].color.b / 0.5f);
     }
 }
